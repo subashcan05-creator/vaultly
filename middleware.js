7f376d3ba6c2313cf8375edd23897e19eccc60bf
subashcan05-creator <subashcan05@gmail.com>
2026-07-05 21:16:30 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,9 @@
-export {};
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [],
+};
